765a509a98f2600c1be17205cd7d8c400cb9d15c
nhat2211 <tranquangnhat2211@gmail.com>
2023-10-23 22:26:50 +0700

    update login feature

---
+++ DataAccess/Repository/StaffMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using AirConditionBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class StaffMemberRepository : IStaffMemberRepository
+    {
+
+        public StaffMember findStaffByEmailAndPassword(string email, string password)
+        {
+            return StaffMemberDao.Instance.findByEmailAndPassword(email, password);
+        }
+    }
+}
